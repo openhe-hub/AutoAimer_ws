be42328da6ca2987cb4926d0a9c9db6b747fcf33
openhe-hub <625529334@qq.com>
2023-02-26 21:39:24 +0800

    2-26-init

---
+++ build/predictor/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sjtu/code/robomaster/AutoAim_ws/src/predictor
+# Build directory: /home/sjtu/code/robomaster/AutoAim_ws/build/predictor
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
